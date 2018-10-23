fc05c4024e4b182c97680dc02fe3203bce19e18c
丁丁 <Kutinging@users.noreply.github.com>
2018-10-23 19:41:40 +0800

    修正版本號錯誤

--- so-net 虛寶兌換遊戲ID自動輸入.user.js
+++ so-net 虛寶兌換遊戲ID自動輸入.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         so-net 虛寶兌換遊戲ID自動輸入
 // @namespace    https://github.com/Kutinging/svp.so-net
-// @version      1.0
+// @version      1.1
 // @description  so-net 虛寶兌換遊戲ID自動輸入，本版本為全遊戲收錄，請自行輸入遊戲ID，PC使用chrome或firefox，Android請使用firefox，請自行安裝Tampermonkey
 // @author       ThanatosDi , S.Dot
 // @match        http://www.svp.so-net.tw/
